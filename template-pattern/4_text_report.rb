32812fe29756f9339a1a94ac05db62c2d5c42c2b
Santu Koley <santu.koley@yottolabs.com>
2015-02-27 14:49:35 +0530

    Hook Method implementation

---
+++ template-pattern/4_text_report.rb
@@ -0,0 +1,17 @@
+class PlainTextReport < Report
+
+  
+  def output_head
+    puts("**** #{@title} ****")
+    puts
+  end
+
+  def output_line(line)
+    puts(line)
+  end
+
+  
+end
+
+report = PlainTextReport.new
+report.output_report
